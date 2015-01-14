148036a1e0b1f04e9f3581807467d81a92eb0d34
james-driscoll <james.e.m.driscoll@gmail.com>
2015-01-14 20:17:50 +0000

    Removed image and replaced with comment.

--- front-page.php
+++ front-page.php
@@ -18,9 +18,7 @@
     get_template_parts( array( 'parts/html-header') );
   ?>
 
-  <a class="home-logo container" href="<?php echo get_site_url(); ?>">
-     <img class="logo" height="576" width="720" src="<?php echo get_stylesheet_directory_uri() ?>/images/logo-home.gif" alt="Thomas Line Art logo">
-  </a>
+  <!-- Jumbotron! -->
 
   <!-- Content ends here -->
 
