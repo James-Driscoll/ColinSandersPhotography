17d9545350b35f01284a797b769113031563f9d2
james-driscoll <james.e.m.driscoll@gmail.com>
2015-01-17 19:26:03 +0000

    Added test images to gallery categories.

--- page-jdtla_work.php
+++ page-jdtla_work.php
@@ -14,10 +14,10 @@ get_template_parts( array( 'parts/html-header') ); ?>
 <div class="container">
 	<div class="page-portfolio">
 
-	<a href="#" class="category"><h2>Motorsport</h2><img src="<?php echo get_stylesheet_directory_uri() ?>/images/.jpg"></a>
-	<a href="#" class="category"><h2>Industry</h2><img src="<?php echo get_stylesheet_directory_uri() ?>/images/.jpg"></a>
-	<a href="#" class="category"><h2>Family</h2><img src="<?php echo get_stylesheet_directory_uri() ?>/images/.jpg"></a>
-	<a href="#" class="category"><h2>Chicken</h2><img src="<?php echo get_stylesheet_directory_uri() ?>/images/.jpg"></a>
+	<a href="#" class="category"><h2>Motorsport</h2><img height="230" width="230" src="<?php echo get_stylesheet_directory_uri() ?>/images/ms.jpg"></a>
+	<a href="#" class="category"><h2>Industry</h2><img height="230" width="230" src="<?php echo get_stylesheet_directory_uri() ?>/images/industry.jpg"></a>
+	<a href="#" class="category"><h2>Family</h2><img height="230" width="230" src="<?php echo get_stylesheet_directory_uri() ?>/images/family.jpg"></a>
+	<a href="#" class="category"><h2>Chicken</h2><img height="230" width="230" src="<?php echo get_stylesheet_directory_uri() ?>/images/chicken.jpg"></a>
 
 	</div>
 </div>
